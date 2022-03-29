34a08e9755c647434a3b722b9167ad0b4a5ab81e
Qi Zhang <qi.zhang@liferay.com>
2022-03-29 10:20:38 +0800

    LPS-150444 Improve logic for more conditions

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/PropertiesEmptyLinesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/PropertiesEmptyLinesCheck.java
@@ -14,11 +14,15 @@
 
 package com.liferay.source.formatter.check;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.IOException;
 
 /**
  * @author Hugo Huijser
@@ -27,77 +31,215 @@ public class PropertiesEmptyLinesCheck extends BaseFileCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, String content) {
+			String fileName, String absolutePath, String content)
+		throws IOException {
 
-		content = _fixMissingEmptyLinesAroundCategory(content);
-
-		if (!fileName.endsWith("/liferay-plugin-package.properties")) {
-			content = _fixMissingEmptyLinesAroundMultiLineProperty(content);
+		if (!fileName.endsWith(".properties")) {
+			return content;
 		}
 
-		return content;
+		return _fixMissingEmptyLines(content);
 	}
 
-	private String _fixMissingEmptyLinesAroundCategory(String content) {
-		Matcher matcher = _missingEmptyLineAfterCategoryPattern.matcher(
-			content);
+	private String _fixMissingEmptyLines(String content) throws IOException {
+		int lineNumber = 0;
 
-		if (matcher.find()) {
-			return StringUtil.replaceFirst(
-				content, "\n", "\n\n", matcher.start(1) - 1);
-		}
+		StringBundler sb = new StringBundler();
 
-		matcher = _missingEmptyLineBeforeCategoryPattern.matcher(content);
+		String previousLine = StringPool.BLANK;
 
-		if (matcher.find()) {
-			return StringUtil.replaceFirst(
-				content, "\n", "\n\n", matcher.end(1));
-		}
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
 
-		return content;
-	}
+			String line;
 
-	private String _fixMissingEmptyLinesAroundMultiLineProperty(
-		String content) {
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				lineNumber++;
 
-		Matcher matcher = _missingEmptyLineAfterMultiLinePattern.matcher(
-			content);
+				if (Validator.isNull(line)) {
+					sb.append(line);
+					sb.append(StringPool.NEW_LINE);
+					previousLine = StringPool.BLANK;
 
-		while (matcher.find()) {
-			String nextLinePropertyKey = StringUtil.extractFirst(
-				matcher.group(4), "=");
+					continue;
+				}
 
-			if (Validator.isNotNull(nextLinePropertyKey)) {
-				continue;
+				String trimLine = StringUtil.trim(line);
+
+				if (StringUtil.equals(trimLine, StringPool.BACK_SLASH) ||
+					StringUtil.equals(trimLine, "#\\")) {
+
+					sb.append(line);
+					sb.append(StringPool.NEW_LINE);
+					previousLine = trimLine;
+
+					continue;
+				}
+
+				if (!trimLine.startsWith("#")) {
+					int index = trimLine.indexOf("#");
+
+					if ((index != -1) && (index < (trimLine.length() - 1)) &&
+						(trimLine.charAt(index + 1) == CharPool.SPACE)) {
+
+						trimLine = StringUtil.trim(
+							trimLine.substring(0, index));
+					}
+				}
+
+				if (!trimLine.startsWith("#") &&
+					(StringUtil.equals(previousLine, "##") ||
+					 StringUtil.equals(previousLine, "#"))) {
+
+					sb.append(StringPool.NEW_LINE);
+					sb.append(line);
+					sb.append(StringPool.NEW_LINE);
+					previousLine = trimLine;
+
+					continue;
+				}
+
+				if (trimLine.startsWith("##") &&
+					Validator.isNotNull(previousLine) &&
+					!previousLine.startsWith("##")) {
+
+					sb.append(StringPool.NEW_LINE);
+					sb.append(line);
+					sb.append(StringPool.NEW_LINE);
+					previousLine = trimLine;
+
+					continue;
+				}
+
+				if (trimLine.matches("[^#]+=\\\\") &&
+					Validator.isNotNull(previousLine)) {
+
+					sb.append(StringPool.NEW_LINE);
+					sb.append(line);
+					sb.append(StringPool.NEW_LINE);
+					previousLine = trimLine;
+
+					continue;
+				}
+
+				String nextLine = getLine(content, lineNumber + 1);
+
+				if (Validator.isNotNull(nextLine)) {
+					nextLine = StringUtil.trim(nextLine);
+				}
+				else {
+					nextLine = StringPool.BLANK;
+				}
+
+				if (!trimLine.startsWith("#") &&
+					!trimLine.endsWith(StringPool.BACK_SLASH) &&
+					previousLine.endsWith(StringPool.BACK_SLASH) &&
+					Validator.isNotNull(nextLine) &&
+					!nextLine.matches("^#(?![ #]).+")) {
+
+					sb.append(line);
+					sb.append(StringPool.NEW_LINE);
+					sb.append(StringPool.NEW_LINE);
+					previousLine = StringPool.BLANK;
+
+					continue;
+				}
+
+				if (trimLine.startsWith("#") &&
+					trimLine.matches(_MULTI_COMMENTS_REGEX)) {
+
+					if (StringUtil.equals(previousLine, "\\") ||
+						StringUtil.equals(previousLine, "#\\") ||
+						previousLine.matches("[^#]+=\\\\*")) {
+
+						sb.append(line);
+						sb.append(StringPool.NEW_LINE);
+						previousLine = trimLine;
+
+						continue;
+					}
+
+					if (Validator.isNotNull(previousLine) &&
+						!previousLine.matches("^((# +.+)|#)$") &&
+						nextLine.matches(_MULTI_COMMENTS_REGEX)) {
+
+						sb.append(StringPool.NEW_LINE);
+						sb.append(line);
+						sb.append(StringPool.NEW_LINE);
+						previousLine = trimLine;
+
+						continue;
+					}
+				}
+
+				if (trimLine.matches("^#(?![ #]).+") &&
+					previousLine.matches(_MULTI_COMMENTS_REGEX) &&
+					Validator.isNotNull(nextLine)) {
+
+					String statement = trimLine.replaceAll(
+						StringPool.DOUBLE_BACK_SLASH, StringPool.BLANK);
+					String nextStatement = nextLine.replaceAll(
+						StringPool.DOUBLE_BACK_SLASH, StringPool.BLANK);
+					statement = statement.substring(1);
+
+					if (nextLine.startsWith("#") && (nextLine.length() > 1)) {
+						nextStatement = nextStatement.substring(1);
+					}
+
+					if (nextLine.endsWith("\\")) {
+						if (StringUtil.equals(statement, nextStatement)) {
+							sb.append(line);
+							sb.append(StringPool.NEW_LINE);
+							sb.append(StringPool.NEW_LINE);
+							previousLine = StringPool.BLANK;
+						}
+						else {
+							sb.append(StringPool.NEW_LINE);
+							sb.append(line);
+							sb.append(StringPool.NEW_LINE);
+							previousLine = trimLine;
+						}
+					}
+					else {
+						String[] statementArray = statement.split(
+							StringPool.EQUAL);
+						String[] nextStatementArray = nextStatement.split(
+							StringPool.EQUAL);
+
+						if ((statementArray.length == 1) &&
+							(nextStatementArray.length == 2) &&
+							StringUtil.equals(
+								statementArray[0], nextStatementArray[0])) {
+
+							sb.append(line);
+							sb.append(StringPool.NEW_LINE);
+							sb.append(StringPool.NEW_LINE);
+							previousLine = StringPool.BLANK;
+						}
+						else {
+							sb.append(StringPool.NEW_LINE);
+							sb.append(line);
+							sb.append(StringPool.NEW_LINE);
+							previousLine = trimLine;
+						}
+					}
+
+					continue;
+				}
+
+				sb.append(line);
+				sb.append(StringPool.NEW_LINE);
+				previousLine = trimLine;
 			}
-
-			String currentPropertyKeyPattern =
-				Pattern.quote(matcher.group(1)) + "#?" +
-					Pattern.quote(matcher.group(2));
-
-			if (!nextLinePropertyKey.matches(currentPropertyKeyPattern)) {
-				return StringUtil.replaceFirst(
-					content, "\n", "\n\n", matcher.start(4) - 1);
-			}
 		}
 
-		matcher = _missingEmptyLineBeforeMultiLinePattern.matcher(content);
-
-		if (matcher.find()) {
-			return StringUtil.replaceFirst(
-				content, "\n", "\n\n", matcher.start(1));
+		if (sb.index() > 0) {
+			sb.setIndex(sb.index() - 1);
 		}
 
-		return content;
+		return sb.toString();
 	}
 
-	private static final Pattern _missingEmptyLineAfterCategoryPattern =
-		Pattern.compile("\n## \\w.*\n##\n([^\n#]|#[^#])");
-	private static final Pattern _missingEmptyLineAfterMultiLinePattern =
-		Pattern.compile("( *)([^#\n].*?)=\\\\(\n\\1 +.+)++\n(.+)");
-	private static final Pattern _missingEmptyLineBeforeCategoryPattern =
-		Pattern.compile("\n([^\n#]|#[^#]).*\n##\n");
-	private static final Pattern _missingEmptyLineBeforeMultiLinePattern =
-		Pattern.compile("\n *(?![ #]).+(\n(?!\n) *[^#].*=\\\\\n)");
+	private static final String _MULTI_COMMENTS_REGEX = "^((# (?! ).+)|#)$";
 
 }
