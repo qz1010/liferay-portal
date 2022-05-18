93ac73cc2c9e217a22966b7786221bf17614d0d6
Qi Zhang <qi.zhang@liferay.com>
2022-05-18 14:22:16 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/GroupPagesPortlet.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/GroupPagesPortlet.java
@@ -105,7 +105,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.name=" + LayoutAdminPortletKeys.GROUP_PAGES,
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.supported-public-render-parameter=layoutSetBranchId",
-		"javax.portlet.supported-public-render-parameter=selPlid"
+		"javax.portlet.supported-public-render-parameter=selPlid",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
