d6a3dda26fb875bb7d0e042ccab6d64c301ca0fb
Alan Huang <alan.huang@liferay.com>
2022-07-11 03:26:26 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/hello-velocity/hello-velocity-web/src/main/java/com/liferay/hello/velocity/web/internal/portlet/HelloVelocityPortlet.java
+++ modules/apps/hello-velocity/hello-velocity-web/src/main/java/com/liferay/hello/velocity/web/internal/portlet/HelloVelocityPortlet.java
@@ -57,7 +57,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.portlet.info.short-title=Hello Velocity",
 		"javax.portlet.portlet.info.title=Hello Velocity",
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
