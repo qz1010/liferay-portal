eb5ccac12253f4fdb9a082f797ac9e11966fc71f
Qi Zhang <qi.zhang@liferay.com>
2023-12-28 13:07:44 +0800

    LPS-204735 Enforce use the setter with the UnsafeSupplier

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/InstanceInitializerCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/InstanceInitializerCheck.java
@@ -30,8 +30,6 @@ import java.io.IOException;
 
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Alan Huang
@@ -87,13 +85,27 @@ public class InstanceInitializerCheck extends BaseCheck {
 			return;
 		}
 
-		_checkSetCall(firstChildDetailAST, exprDetailASTList, javaClass);
+		DetailAST childDetailAST = firstChildDetailAST.getFirstChild();
 
-		for (DetailAST literalIfDetailAST :
-				getAllChildTokens(
-					firstChildDetailAST, false, TokenTypes.LITERAL_IF)) {
+		while (childDetailAST != null) {
+			int tokenType = childDetailAST.getType();
 
-			_checkIfStatement(literalIfDetailAST, javaClass);
+			if (tokenType == TokenTypes.LITERAL_IF) {
+				DetailAST slistDetailAST = childDetailAST.findFirstToken(
+					TokenTypes.SLIST);
+
+				for (DetailAST exprDetailAST :
+						getAllChildTokens(
+							slistDetailAST, false, TokenTypes.EXPR)) {
+
+					_checkExprStatement(exprDetailAST, javaClass, true);
+				}
+			}
+			else if (tokenType == TokenTypes.EXPR) {
+				_checkExprStatement(childDetailAST, javaClass, false);
+			}
+
+			childDetailAST = childDetailAST.getNextSibling();
 		}
 	}
 
@@ -157,8 +169,73 @@ public class InstanceInitializerCheck extends BaseCheck {
 		}
 	}
 
+	private void _checkExprStatement(
+		DetailAST exprDetailAST, JavaClass javaClass, boolean inIfBody) {
+
+		DetailAST firstChildDetailAST = exprDetailAST.getFirstChild();
+
+		if (firstChildDetailAST == null) {
+			return;
+		}
+
+		if (firstChildDetailAST.getType() == TokenTypes.ASSIGN) {
+			firstChildDetailAST = firstChildDetailAST.getFirstChild();
+
+			if (firstChildDetailAST.getType() != TokenTypes.IDENT) {
+				return;
+			}
+
+			String variableName = firstChildDetailAST.getText();
+
+			String methodName =
+				"set" + StringUtil.upperCaseFirstLetter(variableName);
+
+			_checkHasReplacableMethodSignature(
+				firstChildDetailAST, methodName, javaClass, inIfBody);
+		}
+		else if (firstChildDetailAST.getType() == TokenTypes.METHOD_CALL) {
+			DetailAST dotDetailAST = firstChildDetailAST.findFirstToken(
+				TokenTypes.DOT);
+
+			if (dotDetailAST != null) {
+				return;
+			}
+
+			String methodName = getMethodName(firstChildDetailAST);
+
+			if (!methodName.startsWith("set")) {
+				return;
+			}
+
+			if (inIfBody) {
+				_checkHasReplacableMethodSignature(
+					firstChildDetailAST, methodName, javaClass, true);
+
+				return;
+			}
+
+			DetailAST elistDetailAST = firstChildDetailAST.findFirstToken(
+				TokenTypes.ELIST);
+
+			DetailAST childDetailAST = elistDetailAST.getFirstChild();
+
+			if (childDetailAST == null) {
+				return;
+			}
+
+			if ((childDetailAST.getType() != TokenTypes.LAMBDA) &&
+				(childDetailAST.findFirstToken(TokenTypes.METHOD_REF) ==
+					null)) {
+
+				_checkHasReplacableMethodSignature(
+					firstChildDetailAST, methodName, javaClass, inIfBody);
+			}
+		}
+	}
+
 	private void _checkHasReplacableMethodSignature(
-		DetailAST detailAST, String methodName, JavaClass javaClass) {
+		DetailAST detailAST, String methodName, JavaClass javaClass,
+		boolean inIfBody) {
 
 		for (JavaTerm javaTerm : javaClass.getChildJavaTerms()) {
 			if (!javaTerm.isJavaMethod() || javaTerm.isPrivate()) {
@@ -184,134 +261,19 @@ public class InstanceInitializerCheck extends BaseCheck {
 			String parameterType = javaParameter.getParameterType();
 
 			if (parameterType.startsWith("UnsafeSupplier")) {
-				if (detailAST.getType() == TokenTypes.METHOD_CALL) {
+				if ((detailAST.getType() == TokenTypes.METHOD_CALL) &&
+					inIfBody) {
+
 					log(detailAST, _MSG_INLINE_IF_STATEMENT, methodName);
+
+					return;
 				}
-				else {
-					log(
-						detailAST, _MSG_USE_SET_METHOD_INSTEAD, methodName,
-						parameterType);
-				}
+
+				log(
+					detailAST, _MSG_USE_SET_METHOD_INSTEAD, methodName,
+					parameterType);
 
 				return;
-			}
-		}
-	}
-
-	private void _checkIfStatement(
-		DetailAST literalIfDetailAST, JavaClass javaClass) {
-
-		DetailAST slistDetailAST = literalIfDetailAST.findFirstToken(
-			TokenTypes.SLIST);
-
-		List<DetailAST> exprDetailASTList = getAllChildTokens(
-			slistDetailAST, false, TokenTypes.EXPR);
-
-		for (DetailAST exprDetailAST : exprDetailASTList) {
-			DetailAST firstChildDetailAST = exprDetailAST.getFirstChild();
-
-			if (firstChildDetailAST == null) {
-				continue;
-			}
-
-			if (firstChildDetailAST.getType() == TokenTypes.ASSIGN) {
-				firstChildDetailAST = firstChildDetailAST.getFirstChild();
-
-				if (firstChildDetailAST.getType() != TokenTypes.IDENT) {
-					continue;
-				}
-
-				String variableName = firstChildDetailAST.getText();
-
-				String methodName =
-					"set" + StringUtil.upperCaseFirstLetter(variableName);
-
-				_checkHasReplacableMethodSignature(
-					firstChildDetailAST, methodName, javaClass);
-			}
-			else if (firstChildDetailAST.getType() == TokenTypes.METHOD_CALL) {
-				DetailAST dotDetailAST = firstChildDetailAST.findFirstToken(
-					TokenTypes.DOT);
-
-				if (dotDetailAST != null) {
-					continue;
-				}
-
-				String methodName = getMethodName(firstChildDetailAST);
-
-				if (!methodName.startsWith("set")) {
-					continue;
-				}
-
-				_checkHasReplacableMethodSignature(
-					firstChildDetailAST, methodName, javaClass);
-			}
-		}
-	}
-
-	private void _checkSetCall(
-		DetailAST detailAST, List<DetailAST> exprDetailASTList,
-		JavaClass javaClass) {
-
-		for (DetailAST exprDetailAST : exprDetailASTList) {
-			DetailAST firstChildDetailAST = exprDetailAST.getFirstChild();
-
-			if (firstChildDetailAST.getType() != TokenTypes.METHOD_CALL) {
-				continue;
-			}
-
-			DetailAST dotDetailAST = firstChildDetailAST.findFirstToken(
-				TokenTypes.DOT);
-
-			if (dotDetailAST != null) {
-				continue;
-			}
-
-			int startLineNumber = getStartLineNumber(firstChildDetailAST);
-
-			String methodName = getMethodName(firstChildDetailAST);
-
-			if (!methodName.matches("set[A-Z]\\w*")) {
-				continue;
-			}
-
-			DetailAST elistDetailAST = firstChildDetailAST.findFirstToken(
-				TokenTypes.ELIST);
-
-			firstChildDetailAST = elistDetailAST.getFirstChild();
-
-			if ((firstChildDetailAST == null) ||
-				(firstChildDetailAST.getType() != TokenTypes.EXPR)) {
-
-				continue;
-			}
-
-			String variableName = StringUtil.lowerCaseFirstLetter(
-				methodName.substring(3));
-
-			List<String> names = getNames(detailAST, true);
-
-			if (names.contains(variableName)) {
-				continue;
-			}
-
-			Pattern pattern = Pattern.compile(
-				"\\s(\\S+)\\s+(\\S+\\.)?" + variableName);
-
-			for (JavaTerm javaTerm : javaClass.getChildJavaTerms()) {
-				if (!javaTerm.isJavaVariable() || javaTerm.isPrivate()) {
-					continue;
-				}
-
-				Matcher matcher = pattern.matcher(javaTerm.getContent());
-
-				if (matcher.find()) {
-					log(
-						startLineNumber, _MSG_USE_ASSIGN_INSTEAD,
-						javaTerm.getName(), methodName);
-
-					break;
-				}
 			}
 		}
 	}
@@ -385,8 +347,6 @@ public class InstanceInitializerCheck extends BaseCheck {
 
 	private static final String _MSG_MOVE_ASSIGN_BEFORE_METHOD_CALL =
 		"assign.move.before.method.call";
-
-	private static final String _MSG_USE_ASSIGN_INSTEAD = "assign.use.instead";
 
 	private static final String _MSG_USE_SET_METHOD_INSTEAD =
 		"set.method.use.instead";
